e58c961462852b2ede707ed815250c0d43bde6fa
girishnaidu5555-lgtm <girishnaidu5555@gmail.com>
2026-05-05 00:41:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,9 +6,10 @@ import joblib
 import streamlit as st
 import time
 
-st.write(os.listdir(BASE_DIR))
+
 BASE_DIR = os.path.dirname(__file__)
 model_path = os.path.join(BASE_DIR, "sentiment_pipeline.pkl")
+st.write(os.listdir(BASE_DIR))
 
 model = joblib.load(model_path)
     
