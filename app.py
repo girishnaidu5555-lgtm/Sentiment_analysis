9819c9ecd531d9faeec3d8fcaaa3538ceda16e3a
girishnaidu5555-lgtm <girishnaidu5555@gmail.com>
2026-05-05 00:12:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,12 +3,10 @@ import streamlit as st
 import os
 import time
 
-
 BASE_DIR = os.path.dirname(__file__)
-model_path = os.path.join(BASE_DIR, "sentiment_analysis", "sentiment_pipeline.pkl")
+model_path = os.path.join(BASE_DIR, "sentiment_pipeline.pkl")
 
-
-model = joblib.load("sentiment_pipeline.pkl")
+model = joblib.load(model_path)
     
 st.set_page_config(page_title="Sentiment Analyzer", page_icon="🎬", layout="centered")
 
